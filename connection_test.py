87ad6aa17f2655f68443f7d3d22f863d135832ff
peter <atrazhev@ualberta.ca>
2019-08-28 21:04:43 -0600

    connecting and logging
    need to fix the code to write every line

--- connection_test.py
+++ connection_test.py
@@ -1,17 +1,23 @@
-from connect_to_board import Modbusconnection
+# from connect_to_board import Modbusconnection
 import urllib.request, json
 import time
+import csv 
 
 if __name__ == "__main__":
     # test = Modbusconnection()
     # flag = test.connect_modbus()
     # print(flag)
 
-
-
-    # this code is able to get the Current JSON information from the server 
-    with urllib.request.urlopen("http://192.168.1.2/JsonUpdateParamIdValues.cgi?"+str(time.time())) as url:
-        data = json.loads(url.read().decode())
-        # data = url.read().decode()
-        print(data)
-        print(type(data))
+    while 1:
+        # this code is able to get the Current JSON information from the server 
+        with urllib.request.urlopen("http://192.168.1.2/JsonUpdateParamIdValues.cgi?"+str(time.time())) as url:
+            data = json.loads(url.read().decode())
+            # data = url.read().decode()
+            # print(data)
+            # print(type(data))
+            print(data['id1'])
+            with open('test_charging_data3.csv', mode = 'w') as data_file:
+                data_writer = csv.writer(data_file, delimiter=',', quotechar = '"', quoting=csv.QUOTE_MINIMAL)
+                data_writer.writerow([str(time.time()), str(data['id1'])])
+    
+        
