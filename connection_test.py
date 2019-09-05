7fa1377923f01c78e6fa0967c037788c99960829
Peter Atrazhev <atrazhev@ualberta.ca>
2019-09-05 11:52:26 -0600

    try me
    i think the loops work

--- connection_test.py
+++ connection_test.py
@@ -8,18 +8,27 @@ if __name__ == "__main__":
     # flag = test.connect_modbus()
     # print(flag)
 
-    while 1:
-        # this code is able to get the Current JSON information from the server 
-        with urllib.request.urlopen("http://192.168.1.2/JsonUpdateParamIdValues.cgi?"+str(time.time())) as url:
-            data = json.loads(url.read().decode())
-            # data = url.read().decode()
-            # print(data)
-            # print(type(data))
-            print(data['id1'])
+keys = ['id1', 'id83','id87','id90']
+
+
+
+with open('test_charging_data3.csv', mode = 'w') as data_file:
+    data_writer = csv.writer(data_file, delimiter=',', quotechar = '"', quoting=csv.QUOTE_MINIMAL)
+    with urllib.request.urlopen("http://192.168.1.2/JsonUpdateParamIdValues.cgi?"+str(time.time())) as url:
+        while 1:
+            # this code is able to get the Current JSON information from the server 
+                data = json.loads(url.read().decode()) 
+                # data = url.read().decode()
+                # print(data)
+                # print(type(data))
+                print(data['id1'])
+                # data_writer.writerow([str(time.time()), str(data['id1'])])
+
+              
             
             
-        with open('test_charging_data3.csv', mode = 'w') as data_file:
-            data_writer = csv.writer(data_file, delimiter=',', quotechar = '"', quoting=csv.QUOTE_MINIMAL)
-            data_writer.writerow([str(time.time()), str(data['id1'])])
+        
+            
+            
     
         
